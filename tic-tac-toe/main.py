71d633e41a99402812aa5f4136aa2bbae0d40516
oriont <elijahotarr@gmail.com>
2021-06-27 16:36:13 -0700

    fix logging for win percentages of minimax and random strategies

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -8,11 +8,8 @@ import time
 start = time.time()
 last_log = time.time()
 print("Starting")
-win_counter = {
-    1: 0,
-    0: 0,
-    -1: 0
-}
+# Win counter is [Minimax, Random, Ties]
+win_counter = [0, 0, 0]
 games_passed = 0
 
 # While 3 minutes haven't passed yet
@@ -22,15 +19,29 @@ while time.time() - start < 3 * 60:
     # Play games
     game = Game(strats)
     winner = game.play_game()
-    win_counter[winner] += 1
+    if winner == 1 and strats[0] == MinimaxStrategy or\
+        winner == -1 and strats[1] == MinimaxStrategy:
+        winning_strat = 0
+    elif winner == 1 and strats[0] == RandomStrategy or\
+        winner == -1 and strats[1] == RandomStrategy:
+        winning_strat = 1
+    else:
+        winning_strat = 2
+    win_counter[winning_strat] += 1
 
     # Alternate strategies
     strats = strats[::-1]
 
     # Periodic logs
-    if (time.time() - start) % 10 < .1:
+    if time.time() - last_log > 15:
         print("Games passed:", games_passed)
+        last_log = time.time()
+
 
 print("======")
+print("Games passed:", games_passed)
+print("Win counter format: [Minimax, Random, Tie]")
 print("Final wins after 3 minutes:")
 print(win_counter)
+print("Win percents after 3 minutes:")
+print([v/games_passed for v in win_counter])
